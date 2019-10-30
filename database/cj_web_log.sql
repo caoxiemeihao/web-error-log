10e2022c68a745bbc64ecddcb37b82dd8de3035b
草鞋没号 <308487730@qq.com>
2019-10-30 19:39:49 +0800

    修改字段

--- database/cj_web_log.sql
+++ database/cj_web_log.sql
@@ -1,32 +1,36 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50506
-Source Host           : localhost:3306
-Source Database       : cj_web_log
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50506
+ Source Host           : localhost:3306
+ Source Schema         : cj_web_log
 
-Target Server Type    : MYSQL
-Target Server Version : 50506
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50506
+ File Encoding         : 65001
 
-Date: 2019-10-30 18:49:11
+ Date: 30/10/2019 19:39:31
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for err_log
 -- ----------------------------
 DROP TABLE IF EXISTS `err_log`;
-CREATE TABLE `err_log` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `type` varchar(255) DEFAULT NULL,
-  `title` text,
-  `content` text,
-  `access_url` varchar(255) DEFAULT NULL,
-  `params` varchar(255) DEFAULT NULL,
-  `add_time` datetime DEFAULT NULL,
-  `create_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+CREATE TABLE `err_log`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `title` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `access_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `params` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `add_time` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
